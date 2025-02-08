a2f55011fbc0aa7d48ac65e56339a9f6c91fdfe8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-02-08 14:55:20 +0700

    update

--- public_html/assets/js/script.js
+++ public_html/assets/js/script.js
@@ -1,38 +1,35 @@
 const form = document.getElementById("form");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    const url = e.target.action;
-    const formData = new FormData(form);
+  const url = e.target.action;
+  const formData = new FormData(form);
 
-    fetch(url, {
-        method: 'post',
-        body: formData,
-        mode: 'no-cors'
-    }).then(() => {
-        window.location.href = '/public_html/thanks.html'
-    })
-})
+  fetch(url, {
+    method: "post",
+    body: formData,
+    mode: "no-cors",
+  }).then(() => {
+    window.location.href = "my-portfolio/public_html/thanks.html";
+  });
+});
 
 document.querySelectorAll(".filter-btn").forEach((button) => {
-        button.addEventListener("click", () => {
-          const filter = button.getAttribute("data-filter");
+  button.addEventListener("click", () => {
+    const filter = button.getAttribute("data-filter");
 
-          document
-            .querySelectorAll(".filter-btn")
-            .forEach((btn) => btn.classList.remove("btn-primary"));
-          button.classList.add("btn-primary");
+    document
+      .querySelectorAll(".filter-btn")
+      .forEach((btn) => btn.classList.remove("btn-primary"));
+    button.classList.add("btn-primary");
 
-          document.querySelectorAll(".task-item").forEach((item) => {
-            if (
-              filter === "all" ||
-              item.getAttribute("data-category") === filter
-            ) {
-              item.style.display = "block";
-            } else {
-              item.style.display = "none";
-            }
-          });
-        });
-      });
+    document.querySelectorAll(".task-item").forEach((item) => {
+      if (filter === "all" || item.getAttribute("data-category") === filter) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+});
